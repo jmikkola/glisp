86d6c5f46f5929ceed29b4e8726131055be1abb5
Jeremy Mikkola <jeremy@jeremymikkola.com>
2013-10-05 10:57:47 -0700

    Add some basic types

--- glisp.go
+++ glisp.go
@@ -6,18 +6,50 @@ import (
 	"os"
 )
 
-func main() {
-	var filename string
+const (
+	TYPE_CONS = iota
+	TYPE_VALUE
+)
 
+type SExpression interface {
+	ExprType() int
+}
+
+type ConsCell struct {
+	Car, Cdr *SExpression
+}
+
+func (cons *ConsCell) ExprType() int {
+	return TYPE_CONS
+}
+
+type Value struct {
+	Val string
+}
+
+func (val *Value) ExprType() int {
+	return TYPE_VALUE
+}
+
+func readFile() (s string, err error) {
+	var content []byte
+	var filename string
 	if len(os.Args) > 1 {
 		filename = os.Args[1]
 	}
-
-	content, err := ioutil.ReadFile(filename)
+	content, err = ioutil.ReadFile(filename)
 	if err == nil {
-		s := string(content)
-		fmt.Println(s)
-	} else {
-		fmt.Println(err)
+		s = string(content)
 	}
+	return
+}
+
+func main() {
+	var sxp SExpression
+
+	sxp = &ConsCell{}
+	fmt.Println(sxp.ExprType())
+
+	sxp = &Value{"val"}
+	fmt.Println(sxp.ExprType())
 }

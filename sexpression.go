c3f5c805900a0e2fff912cbc48fa61e88fa51827
Jeremy Mikkola <jeremy@jeremymikkola.com>
2013-10-05 15:10:17 -0700

    Working on making this be a proper module

---
+++ sexpression.go
@@ -0,0 +1,51 @@
+package main
+
+const (
+	TYPE_CONS = iota
+	TYPE_VALUE
+)
+
+type SExpression interface {
+	ExprType() int
+	ToString() string
+}
+
+type ConsCell struct {
+	Car SExpression
+	Cdr *ConsCell
+}
+
+func (cons *ConsCell) ExprType() int {
+	return TYPE_CONS
+}
+
+func (cons *ConsCell) ToString() string {
+	s := "("
+
+	for ; cons != nil; cons = cons.Cdr {
+		if cons.Car != nil {
+			s += cons.Car.ToString()
+		} else {
+			s += "nil"
+		}
+
+		if cons.Cdr != nil {
+			s += " "
+		}
+	}
+
+	s += ")"
+	return s
+}
+
+type Value struct {
+	Val string
+}
+
+func (val *Value) ExprType() int {
+	return TYPE_VALUE
+}
+
+func (val *Value) ToString() string {
+	return val.Val
+}

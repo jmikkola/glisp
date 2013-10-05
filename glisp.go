d1eef52fbeafa56e2c1c509895b4f35eae8cc2a8
Jeremy Mikkola <jeremy@jeremymikkola.com>
2013-10-05 12:18:03 -0700

    Add parsing and unparsing

--- glisp.go
+++ glisp.go
@@ -14,14 +14,35 @@ const (
 
 type SExpression interface {
 	ExprType() int
+	ToString() string
 }
 
 type ConsCell struct {
-	Car, Cdr *SExpression
+	Car SExpression
+	Cdr *ConsCell
 }
 
 func (cons *ConsCell) ExprType() int {
 	return TYPE_CONS
+}
+
+func (cons *ConsCell) ToString() string {
+	s := "("
+
+	for ; cons != nil; cons = cons.Cdr {
+		if cons.Car != nil {
+			s += cons.Car.ToString()
+		} else {
+			s += "nil"
+		}
+
+		if cons.Cdr != nil {
+			s += " "
+		}
+	}
+
+	s += ")"
+	return s
 }
 
 type Value struct {
@@ -32,19 +53,70 @@ func (val *Value) ExprType() int {
 	return TYPE_VALUE
 }
 
-func parse(input chan rune) (SExpression, error) {
-	chr, ok := <-input
-	if !ok {
-		return nil, errors.New("Unexpected end of input")
+func (val *Value) ToString() string {
+	return val.Val
+}
+
+func isWhiteSpace(b byte) bool {
+	return b == ' ' || b == '\n' || b == '\t' || b == '\r'
+}
+
+func parseList(bytes []byte) (expr SExpression, rest []byte, err error) {
+	var out *ConsCell = nil
+	items := []SExpression{}
+
+	for len(bytes) > 0 && bytes[0] != ')' {
+		item, bx, err := parse(bytes)
+		if err != nil {
+			return nil, nil, err
+		}
+		items = append(items, item)
+		bytes = bx
 	}
 
-	for isWhiteSpace(chr) {
-		chr, ok = <-input
-		if !ok {
-			return nil, errors.New("Unexpected end of input")
-		}
+	if len(bytes) < 1 {
+		return nil, nil, errors.New("Unended list")
 	}
-	return nil, nil
+
+	for i := len(items) - 1; i >= 0; i-- {
+		out = &ConsCell{Car: items[i], Cdr: out}
+	}
+
+	return out, bytes, nil
+}
+
+func parseValue(bytes []byte) (expr SExpression, rest []byte, err error) {
+	i, size := 0, len(bytes)
+	valBytes := []byte{}
+
+	for ; i < size && bytes[i] != ')'; i++ {
+		valBytes = append(valBytes, bytes[i])
+	}
+
+	if len(valBytes) < 1 {
+		return nil, nil, errors.New("Value missing")
+	}
+
+	return &Value{string(valBytes)}, bytes[i-1:], nil
+}
+
+func parse(bytes []byte) (expr SExpression, rest []byte, err error) {
+	i, size := 0, len(bytes)
+	for i < size && isWhiteSpace(bytes[i]) {
+		i++
+	}
+
+	if i >= size {
+		return nil, nil, errors.New("Unexpected end of input")
+	}
+
+	if bytes[i] == '(' {
+		return parseList(bytes[i:])
+	} else if bytes[i] == ')' {
+		return nil, nil, errors.New("Unexpected end of list")
+	}
+
+	return parseValue(bytes[i:])
 }
 
 func readFile() (s string, err error) {
@@ -63,9 +135,9 @@ func readFile() (s string, err error) {
 func main() {
 	var sxp SExpression
 
-	sxp = &ConsCell{}
-	fmt.Println(sxp.ExprType())
+	sxp = &ConsCell{Car: &Value{"+"}, Cdr: &ConsCell{}}
+	fmt.Println(sxp.ExprType(), sxp.ToString())
 
 	sxp = &Value{"val"}
-	fmt.Println(sxp.ExprType())
+	fmt.Println(sxp.ExprType(), sxp.ToString())
 }

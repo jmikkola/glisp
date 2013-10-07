e93d2cf1b519bec3614ec316cd80f70eaeb4c8fe
Jeremy Mikkola <jeremy@jeremymikkola.com>
2013-10-06 20:33:38 -0700

    Refactor

--- evaluate.go
+++ evaluate.go
@@ -42,7 +42,7 @@ func argList(cons *ConsCell) ([]SExpression, error) {
 	return args, nil
 }
 
-func floatArgs(args []SExpression) ([]float64, error) {
+func floatFunc(args []SExpression, minArgs int, fname string, fn func(args []float64) float64) (SExpression, error) {
 	floatArgs := []float64{}
 
 	for i, value := range args {
@@ -56,67 +56,48 @@ func floatArgs(args []SExpression) ([]float64, error) {
 		}
 	}
 
-	return floatArgs, nil
+	if len(floatArgs) < minArgs {
+		return nil, errors.New(fmt.Sprintf("%s requires at least %d args, %d given", fname, minArgs, len(floatArgs)))
+	}
+
+	return &Atom{TYPE_FLOAT, GLFloat(fn(floatArgs))}, nil
 }
 
 var builtins map[string]func(args []SExpression) (SExpression, error) = map[string]func(args []SExpression) (SExpression, error){
 	"+": func(args []SExpression) (SExpression, error) {
-		floats, err := floatArgs(args)
-		if err != nil {
-			return nil, err
-		}
-
-		sum := 0.0
-		for _, val := range floats {
-			sum += val
-		}
-
-		return &Atom{TYPE_FLOAT, GLFloat(sum)}, nil
+		return floatFunc(args, 1, "+", func(args []float64) (sum float64) {
+			for _, val := range args {
+				sum += val
+			}
+			return
+		})
 	},
 	"-": func(args []SExpression) (SExpression, error) {
-		floats, err := floatArgs(args)
-		if err != nil {
-			return nil, err
-		}
-		if len(floats) < 2 {
-			return nil, errors.New("Cannot subtract with fewer than two numbers")
-		}
-
-		sum := floats[0]
-		for _, val := range floats[1:] {
-			sum -= val
-		}
-
-		return &Atom{TYPE_FLOAT, GLFloat(sum)}, nil
+		return floatFunc(args, 2, "-", func(args []float64) (sum float64) {
+			sum = args[0]
+			for _, val := range args[1:] {
+				sum -= val
+			}
+			return
+		})
 	},
 	"*": func(args []SExpression) (SExpression, error) {
-		floats, err := floatArgs(args)
-		if err != nil {
-			return nil, err
-		}
-
-		product := 1.0
-		for _, val := range floats {
-			product *= val
-		}
-
-		return &Atom{TYPE_FLOAT, GLFloat(product)}, nil
+		return floatFunc(args, 1, "*", func(args []float64) (product float64) {
+			product = 1.0
+			for _, val := range args {
+				product *= val
+			}
+			return
+		})
 	},
 	"/": func(args []SExpression) (SExpression, error) {
-		floats, err := floatArgs(args)
-		if err != nil {
-			return nil, err
-		}
-		if len(floats) < 2 {
-			return nil, errors.New("Cannot divide with fewer than two numbers")
-		}
-
-		numerator := floats[0]
-		for _, val := range floats[1:] {
-			numerator /= val
-		}
-
-		return &Atom{TYPE_FLOAT, GLFloat(numerator)}, nil
+		return floatFunc(args, 2, "/", func(args []float64) (numerator float64) {
+			numerator = args[0]
+			for _, val := range args[1:] {
+				numerator /= val
+			}
+			return
+		})
 	},
 	"list": func(args []SExpression) (SExpression, error) {
 		var out *ConsCell = nil
